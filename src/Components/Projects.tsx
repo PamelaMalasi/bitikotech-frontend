f6e56e770f2c1f6c0145aea704da77fcffbb458b
Pamela Malasi <pamelamalasi@Pamelas-MacBook-Pro.local>
2026-03-06 13:43:54 +0100

    fixed the import in project.tsx

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,13 +1,14 @@
 import { motion } from "framer-motion";
-import PortfolioCard from "../Components/PortfolioCard";
-import portfolio1 from "../../assets/portfolio-1.jpg";
-import portfolio2 from "@/assets/portfolio-2.jpg";
-import portfolio3 from "@/assets/portfolio-3.jpg";
-import portfolio4 from "@/assets/portfolio-4.jpg";
-import portfolio5 from "@/assets/portfolio-5.jpg";
-import portfolio6 from "@/assets/portfolio-6.jpg";
-import portfolio7 from "@/assets/portfolio-7.jpg";
-import portfolio8 from "@/assets/portfolio-8.jpg";
+import PortfolioCard from "./PortfolioCard";
+import portfolio1 from "../assets/portfolio-1.jpg";
+import portfolio2 from "../assets/portfolio-2.jpg";
+import portfolio3 from "../assets/portfolio-3.jpg";
+import portfolio4 from "../assets/portfolio-4.jpg";
+import portfolio5 from "../assets/portfolio-5.jpg";
+import portfolio6 from "../assets/portfolio-6.jpg";
+import portfolio7 from "../assets/portfolio-7.jpg";
+import portfolio8 from "../assets/portfolio-8.jpg";
+
 
 const projects = [
   { image: portfolio1, title: "Neon Sphere", category: "3D Art" },
